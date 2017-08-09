c164ade6cc5fa19decf2eb8277dc5cab83ad7bbc
João Ferreira <lubien1996@gmail.com>
2017-08-09 14:25:55 -0300

    Update roman.ex

--- lib/roman.ex
+++ lib/roman.ex
@@ -1,4 +1,35 @@
 defmodule Roman do
+  def from_integer(n), do: from_integer(n, "")
+  defp from_integer(0, acc), do: acc
+  defp from_integer(n, acc) do
+    case proximate_roman_and_value(n) do
+      {roman, value} ->
+        amount = div n, value
+        rest   = rem n, value
+        from_integer(rest, acc <> String.duplicate(roman, amount))
+      roman ->
+        acc <> roman
+    end
+  end
+  
+  defp proximate_roman_and_value(n) do
+    cond do
+      n >= 1000 -> {"M" ,1000}
+      n >= 900  -> {"CM", 900}
+      n >= 500  -> {"D" , 500}
+      n >= 400  -> {"CD", 400}
+      n >= 100  -> {"C" , 100}
+      n >= 90   -> {"XC",  90}
+      n >= 50   -> {"L" ,  50}
+      n >= 40   -> {"XL",  40}
+      n >= 10   -> {"X" ,  10}
+      n == 9    -> {"IX",   9}
+      n >= 5    -> {"V" ,   5}
+      n == 4    -> {"IV",   4}
+      true      -> String.duplicate("I", n)
+    end
+  end
+
   def to_integer(str) do
     str
     |> String.split("")
